89d399ac903f4b4f977d4bd02b1c9055e1185290
William Spongberg <william.spongberg@outlook.com>
2024-11-18 09:10:19 +1100

    Wait until page is fully loaded before scraping

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -1,14 +1,17 @@
-const JOB_DATA_CONTAINER = '.jobs-description__container'
+const JOB_DATA_CONTAINER = ".jobs-description__container";
+let jobData = null;
+
+// wait for the page to load
+window.addEventListener("DOMContentLoaded", function () {
+  grabJobData();
+});
 
 // loop until find the job data container
 function grabJobData() {
-  const jobData = document.querySelector(JOB_DATA_CONTAINER)
-
+  jobData = document.querySelector(JOB_DATA_CONTAINER);
   if (jobData) {
-    console.log(jobData.innerHTML)
+    console.log(jobData.innerHTML);
   } else {
-    setTimeout(grabJobData, 100)
+    console.log("Job data not found");
   }
 }
-
-grabJobData()
