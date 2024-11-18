9dc3ce6ac05130a82f32a297267b77cbfef863b3
Chillerbag <ethanwhawkins@gmail.com>
2024-11-18 19:05:25 +1100

    basic setup of some layout stuff

---
+++ scripts/siteEnabled.js
@@ -0,0 +1,19 @@
+const LINKEDIN_URL = 'https://www.linkedin.com'
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+    if (!tab.url) return;
+    const url = new URL(tab.url);
+    if (url.origin === LINKEDIN_URL) {
+      await chrome.sidePanel.setOptions({
+        tabId,
+        path: 'sidepanel.html',
+        enabled: true
+      });
+    } else {
+      // Disables the side panel on all other sites
+      await chrome.sidePanel.setOptions({
+        tabId,
+        enabled: false
+      });
+    }
+  });
