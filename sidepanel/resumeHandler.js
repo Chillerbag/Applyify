ed78856e70f444184d435164a9cb20be10977083
William Spongberg <william.spongberg@outlook.com>
2024-11-28 13:31:41 +1100

    Tidied up resume handler code

--- sidepanel/resumeHandler.js
+++ sidepanel/resumeHandler.js
@@ -5,8 +5,8 @@ Last modified: 21/11/2024 by Ethan
 */
 
 // -------------------------------------------------------------------
-//                             Declarations 
-// -------------------------------------------------------------------       
+//                             Declarations
+// -------------------------------------------------------------------
 const fileInput = document.getElementById("resumeFile");
 var reader = new FileReader();
 
@@ -20,10 +20,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     const storedData = await chrome.storage.local.get(["resume"]);
 
     if (storedData.resume && Object.keys(storedData.resume).length > 0) {
-        await resumeUploadedChange();
-        await permitResumeGeminiUpdating();
+      await updateResumeUploadStatus(true);
+      permitResumeGeminiUpdating();
+    } else {
+      await updateResumeUploadStatus(false);
     }
-
   } catch {
     errorBoxCreator("body", error);
   }
@@ -31,43 +32,52 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 // on input, trigger a file load.
 fileInput.addEventListener("input", () => {
-    try {
-        const file = document.getElementById("resumeFile").files[0];
-
-        if (file) {
-            reader.readAsText(file, "UTF-8");
-        }
-    } catch (error) {
-        errorBoxCreator("body", error);
-    }
+  loadResume();
 });
 
 // on load, send to local storage and update upload status text.
 reader.addEventListener("load", () => {
-    try {
-        resumeRead = reader.result;
-        chrome.storage.local.set({ resume: resumeRead }).then(async () => {
-            await resumeUploadedChange();
-            await permitResumeGeminiUpdating();
-        });
-    } catch {
-        errorBoxCreator("body", error);
-    }
+  try {
+    resumeRead = reader.result;
+    chrome.storage.local.set({ resume: resumeRead }).then(async () => {
+      await updateResumeUploadStatus(true);
+      permitResumeGeminiUpdating();
+    });
+  } catch {
+    errorBoxCreator("body", error);
+  }
 });
 
 // -------------------------------------------------------------------
 //                            Helper functions
 // -------------------------------------------------------------------
 
-async function resumeUploadedChange() {
+function loadResume() {
+  try {
+    const file = document.getElementById("resumeFile").files[0];
+    if (file) {
+      reader.readAsText(file, "UTF-8");
+    }
+  } catch (error) {
+    errorBoxCreator("body", error);
+  }
+}
+
+async function updateResumeUploadStatus(uploaded) {
   const resumeStatusMsg = document.getElementById("resumeUploadedStatus");
-  resumeStatusMsg.innerHTML = "resume Uploaded!";
-  resumeStatusMsg.className = "resumeUploaded";
-  await chrome.storage.local.set({ resumeUploaded: true });
+  if (uploaded) {
+    resumeStatusMsg.innerHTML = "Resume uploaded";
+    resumeStatusMsg.className = "resumeUploaded";
+    await chrome.storage.local.set({ resumeUploaded: true });
+  } else {
+    resumeStatusMsg.innerHTML = "Resume not uploaded";
+    resumeStatusMsg.className = "resumeNotUploaded";
+    await chrome.storage.local.set({ resumeUploaded: false });
+  }
 }
 
 // tell geminiResponse we can use the resume thing.
-async function permitResumeGeminiUpdating() { 
-    const resumeAllowed = new Event("resumeAvaliable");
-    document.dispatchEvent(resumeAllowed);
+function permitResumeGeminiUpdating() {
+  const resumeAllowed = new Event("resumeAvaliable");
+  document.dispatchEvent(resumeAllowed);
 }
